ce1ce2437b7d33dc9c87d97bab921f3fa43dcc51
Ismail Hassani <ismail.hassani@i-synergy.nl>
2018-11-28 14:23:33 +0100

    Corrected powershell script for CorFlags

--- build/createplatformdll.ps1
+++ build/createplatformdll.ps1
@@ -1,12 +1,7 @@
 param($file)
-
 Write-Host $file
  
 $exe = Get-ChildItem -Path "C:\program files (x86)\Microsoft SDKs\Windows\v10.0A\bin" -Recurse -Include corflags.exe | select -First 1
 Write-Host $exe
- 
-if ($file.Extension -eq ".dll")
-{
-    Write-Host $file.FullName
-    &$exe corflags /32bitreq- $($file.FullName)
-}
+
+&$exe /32bitreq- $file
