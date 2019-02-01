42fa46f17ada3a620d48f4ddea5b7e96061004be
Ismail Hassani <ismail.hassani@i-synergy.nl>
2019-02-01 02:05:10 +0100

    Added UWP Test project

---
+++ tests/I-Synergy.Framework.Windows.Tests/Functions/LocalizationFunctionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ISynergy.Services;
+using ISynergy.Framework.Tests.Base;
+using ISynergy.Functions;
+
+namespace ISynergy.Framework.Tests.Functions
+{
+    [Collection("Localization")]
+    public class LocalizationFunctionsTests : IntegrationTest
+    {
+        private readonly IContext Context;
+        private readonly IBaseSettingsService Settings;
+
+        public LocalizationFunctionsTests()
+        {
+            Context = new Context();
+            Settings = new SettingsService();
+        }
+
+        [Fact]
+        public void CheckIfCultureIsSetToDutchTest()
+        {
+            var x = new LocalizationFunctions(Context, Settings);
+            x.SetLocalizationLanguage("nl");
+
+            Assert.Equal("nl", Settings.Application_Culture);
+        }
+
+        [Fact]
+        public void CheckIfCultureIsSetToEnglishTest()
+        {
+            var x = new LocalizationFunctions(Context, Settings);
+            x.SetLocalizationLanguage("en");
+
+            Assert.Equal("en", Settings.Application_Culture);
+        }
+
+        [Fact]
+        public void CheckIfCurrencyIsSetToEuroTest()
+        {
+            var x = new LocalizationFunctions(Context, Settings);
+            x.SetLocalizationLanguage("nl");
+
+            Assert.Equal("€", Context.CurrencySymbol);
+        }
+
+        [Fact]
+        public void CheckIfCurrencyIsSetToDollarTest()
+        {
+            var x = new LocalizationFunctions(Context, Settings);
+            x.SetLocalizationLanguage("en");
+
+            Assert.Equal("$", Context.CurrencySymbol);
+        }
+    }
+}
