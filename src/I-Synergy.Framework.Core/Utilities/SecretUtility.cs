105d506c6ccba3c77db14164130b58fc86bba42b
Ismail Hassani <ismail.hassani@i-synergy.nl>
2019-03-13 15:22:40 +0100

    Added ApiKey feature
    Related Work Items: #1674

---
+++ src/I-Synergy.Framework.Core/Utilities/SecretUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ISynergy.Utilities
+{
+    public static class SecretUtility
+    {
+        public static string GenerateSecret()
+        {
+            const int byteLength = 66; // 64-bits, round up to get a string without padding in base64.
+            var rng = RandomNumberGenerator.Create();
+
+            var secretBytes = new byte[byteLength];
+            rng.GetNonZeroBytes(secretBytes);
+            return Regex.Replace(Convert.ToBase64String(secretBytes), "[^A-Za-z0-9]", "X");
+        }
+    }
+}
