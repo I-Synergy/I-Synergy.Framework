dd9578043a5ccdd67fc85d77424fede693b59883
Ismail Hassani <ismail.hassani@i-synergy.nl>
2020-05-27 11:25:12 +0200

    Added entities

---
+++ src/I-Synergy.Framework.Entities/Base/BaseInternetAddress.cs
@@ -0,0 +1,25 @@
+﻿using ISynergy.Framework.EntityFramework.Entities;
+using ISynergy.Models.Enumerations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ISynergy.Entities.Base
+{
+    /// <summary>
+    /// Class BaseInternetAddress.
+    /// Implements the <see cref="ISynergy.Framework.EntityFramework.Entities.BaseTenantEntity" />
+    /// </summary>
+    /// <seealso cref="ISynergy.Framework.EntityFramework.Entities.BaseTenantEntity" />
+    public abstract class BaseInternetAddress : BaseTenantEntity
+    {
+        /// <summary>
+        /// Gets or sets the type of the address.
+        /// </summary>
+        /// <value>The type of the address.</value>
+        [Required] public InternetAddressTypes AddressType { get; set; }
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        /// <value>The address.</value>
+        [Required] [StringLength(255)] public string Address { get; set; }
+    }
+}
---
+++ src/I-Synergy.Framework.Entities/Base/BasePhone.cs
@@ -0,0 +1,35 @@
+﻿using ISynergy.Framework.EntityFramework.Entities;
+using ISynergy.Models.Enumerations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ISynergy.Entities.Base
+{
+    /// <summary>
+    /// Class BasePhone.
+    /// Implements the <see cref="ISynergy.Framework.EntityFramework.Entities.BaseTenantEntity" />
+    /// </summary>
+    /// <seealso cref="ISynergy.Framework.EntityFramework.Entities.BaseTenantEntity" />
+    public abstract class BasePhone : BaseTenantEntity
+    {
+        /// <summary>
+        /// Gets or sets the type of the phone.
+        /// </summary>
+        /// <value>The type of the phone.</value>
+        [Required] public PhoneTypes PhoneType { get; set; }
+        /// <summary>
+        /// Gets or sets the country prefix.
+        /// </summary>
+        /// <value>The country prefix.</value>
+        [StringLength(128)] public string CountryPrefix { get; set; }
+        /// <summary>
+        /// Gets or sets the area code.
+        /// </summary>
+        /// <value>The area code.</value>
+        [Required] [StringLength(5)] public string AreaCode { get; set; }
+        /// <summary>
+        /// Gets or sets the subscriber number.
+        /// </summary>
+        /// <value>The subscriber number.</value>
+        [Required] [StringLength(10)] public string SubscriberNumber { get; set; }
+    }
+}
