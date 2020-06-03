bafcd0d99cb954b893ef8e1af4604e56cda35985
Ismail Hassani <ismail.hassani@i-synergy.nl>
2020-06-03 23:55:18 +0200

    Fixed some issues with Guid requirement of Identity

--- src/I-Synergy.Framework.Core/Models/Accounts/User.cs
+++ src/I-Synergy.Framework.Core/Models/Accounts/User.cs
@@ -52,45 +52,6 @@ namespace ISynergy.Framework.Core.Models.Accounts
     }
 
     /// <summary>
-    /// Class Role.
-    /// Implements the <see cref="ModelBase" />
-    /// </summary>
-    /// <seealso cref="ModelBase" />
-    public class Role : ModelBase
-    {
-        /// <summary>
-        /// Gets or sets the Id property value.
-        /// </summary>
-        /// <value>The identifier.</value>
-        [JsonProperty]
-        [Required]
-        public string Id
-        {
-            get { return GetValue<string>(); }
-            set { SetValue(value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the Name property value.
-        /// </summary>
-        /// <value>The name.</value>
-        [JsonProperty]
-        [Required]
-        public string Name
-        {
-            get { return GetValue<string>(); }
-            set { SetValue(value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the description.
-        /// </summary>
-        /// <value>The description.</value>
-        [JsonIgnore]
-        public string Description { get; set; } = string.Empty;
-    }
-
-    /// <summary>
     /// Class UserSelect.
     /// Implements the <see cref="User" />
     /// </summary>
