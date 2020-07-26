e78085d4890b7e24053e329373856ab7ddd7e7a0
Ismail Hassani <ismail.hassani@i-synergy.nl>
2020-07-26 02:08:21 +0200

    Added exception middleware and sample

---
+++ src/I-Synergy.Framework.AspNetCore/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using ISynergy.Framework.Core.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace ISynergy.Framework.AspNetCore.Middleware
+{
+    /// <summary>
+    /// Class ExceptionMiddleware.
+    /// </summary>
+    public class ExceptionMiddleware
+    {
+        /// <summary>
+        /// The next
+        /// </summary>
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">The next.</param>
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// invoke as an asynchronous operation.
+        /// </summary>
+        /// <param name="httpContext">The HTTP context.</param>
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        /// <summary>
+        /// Handles the exception asynchronous.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="exception">The exception.</param>
+        /// <returns>Task.</returns>
+        protected virtual Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return context.Response
+                .WriteAsync(new ErrorDetail(context.Response.StatusCode, exception.Message, exception.GetType().FullName)
+                .ToString());
+        }
+    }
+}
