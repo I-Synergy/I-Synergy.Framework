1b6df2fc457ccd2910fd322284270f5b22517513
Ismail Hassani <ismail.hassani@i-synergy.nl>
2020-10-21 15:38:30 +0200

    Removed obsolete LoadEmbeddedImageResource function

--- src/ISynergy.Framework.UI/Helpers/ResourceHelper.cs
+++ src/ISynergy.Framework.UI/Helpers/ResourceHelper.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Reflection;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Markup;
-using Windows.UI.Xaml.Media.Imaging;
 
 namespace ISynergy.Framework.UI.Helpers
 {
@@ -53,26 +52,6 @@ namespace ISynergy.Framework.UI.Helpers
             stream.Read(bytes, 0, bytes.Length);
 
             return bytes;
-        }
-
-        /// <summary>
-        /// Loads the embedded image resource.
-        /// </summary>
-        /// <param name="type">The type.</param>
-        /// <param name="resourcePath">The resource path.</param>
-        /// <returns>BitmapImage.</returns>
-        public static BitmapImage LoadEmbeddedImageResource(Type type, string resourcePath)
-        {
-            var assembly = type.GetTypeInfo().Assembly;
-            using var stream = assembly.GetManifestResourceStream(resourcePath);
-            var buffer = new MemoryStream();
-            stream.CopyTo(buffer);
-            buffer.Seek(0, SeekOrigin.Begin);
-
-            var image = new BitmapImage();
-            image.SetSource(buffer.AsRandomAccessStream());
-
-            return image;
         }
     }
 }
