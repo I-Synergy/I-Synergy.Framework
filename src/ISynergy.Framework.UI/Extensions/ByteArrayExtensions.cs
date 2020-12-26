1e8602f21b72521ea4c6c5a83a6fdbe2d4936bbb
Ismail Hassani <ismail.hassani@i-synergy.nl>
2020-12-26 17:15:32 +0100

    Fixed issue with ImageSource from Stream

--- src/ISynergy.Framework.UI/Extensions/ByteArrayExtensions.cs
+++ src/ISynergy.Framework.UI/Extensions/ByteArrayExtensions.cs
@@ -1,9 +1,9 @@
 ﻿using ISynergy.Framework.Core.Extensions;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 
 #if NETFX_CORE
 using System.IO;
-using Windows.UI.Xaml.Media.Imaging;
 #endif
 
 namespace ISynergy.Framework.UI.Extensions
@@ -18,16 +18,16 @@ namespace ISynergy.Framework.UI.Extensions
         /// </summary>
         /// <param name="_self">The image.</param>
         /// <returns>ImageSource.</returns>
-#if NETFX_CORE
+
         public static ImageSource ToImageSource(this byte[] _self)
         {
             var bitmap = new BitmapImage();
+#if NETFX_CORE
             bitmap.SetSource(_self.ToMemoryStream().AsRandomAccessStream());
+#else
+            bitmap.SetSource(_self.ToMemoryStream());
+#endif
             return bitmap;
         }
-#else
-        public static ImageSource ToImageSource(this byte[] _self) =>
-            new ImageSource(_self.ToMemoryStream());
-#endif
     }
 }
