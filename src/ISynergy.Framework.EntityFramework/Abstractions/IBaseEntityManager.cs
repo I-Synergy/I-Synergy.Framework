e3ecbbc7bff37a9a48c1e70405113d10b2da62fd
Ismail Hassani <ismail.hassani@i-synergy.nl>
2022-09-29 11:40:58 +0200

    Removed mandatory user parameter

--- src/ISynergy.Framework.EntityFramework/Abstractions/IBaseEntityManager.cs
+++ src/ISynergy.Framework.EntityFramework/Abstractions/IBaseEntityManager.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using ISynergy.Framework.Core.Base;
+using System;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using ISynergy.Framework.Core.Base;
 
 namespace ISynergy.Framework.EntityFramework.Abstractions
 {
@@ -17,24 +17,24 @@ namespace ISynergy.Framework.EntityFramework.Abstractions
         /// <typeparam name="TEntity">The type of the t entity.</typeparam>
         /// <typeparam name="TSource">The type of the t source.</typeparam>
         /// <param name="e">The e.</param>
-        /// <param name="user">The user.</param>
         /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <returns>Task&lt;System.Int32&gt;.</returns>
-        Task<int> AddItemAsync<TEntity, TSource>(TSource e, string user, CancellationToken cancellationToken = default)
+        Task<int> AddItemAsync<TEntity, TSource>(TSource e, CancellationToken cancellationToken = default)
             where TEntity : EntityBase, new()
             where TSource : ModelBase, new();
+
         /// <summary>
         /// Adds the update item asynchronous.
         /// </summary>
         /// <typeparam name="TEntity">The type of the t entity.</typeparam>
         /// <typeparam name="TSource">The type of the t source.</typeparam>
         /// <param name="e">The e.</param>
-        /// <param name="user">The user.</param>
         /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <returns>Task&lt;System.Int32&gt;.</returns>
-        Task<int> AddUpdateItemAsync<TEntity, TSource>(TSource e, string user, CancellationToken cancellationToken = default)
+        Task<int> AddUpdateItemAsync<TEntity, TSource>(TSource e, CancellationToken cancellationToken = default)
             where TEntity : EntityBase, new()
             where TSource : ModelBase, new();
+
         /// <summary>
         /// Existses the asynchronous.
         /// </summary>
@@ -43,6 +43,7 @@ namespace ISynergy.Framework.EntityFramework.Abstractions
         /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <returns>Task&lt;System.Boolean&gt;.</returns>
         Task<bool> ExistsAsync<TEntity>(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default) where TEntity : EntityBase, new();
+
         /// <summary>
         /// Gets the item by identifier asynchronous.
         /// </summary>
@@ -54,29 +55,29 @@ namespace ISynergy.Framework.EntityFramework.Abstractions
         ValueTask<TEntity> GetItemByIdAsync<TEntity, TId>(TId id, CancellationToken cancellationToken = default)
             where TEntity : EntityBase, new()
             where TId : struct;
+
         /// <summary>
         /// Removes the item asynchronous.
         /// </summary>
         /// <typeparam name="TEntity">The type of the t entity.</typeparam>
         /// <typeparam name="TId">The type of the t identifier.</typeparam>
         /// <param name="id">The identifier.</param>
-        /// <param name="user">The user.</param>
         /// <param name="soft">if set to <c>true</c> [soft].</param>
         /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <returns>Task&lt;System.Int32&gt;.</returns>
-        Task<int> RemoveItemAsync<TEntity, TId>(TId id, string user, bool soft = false, CancellationToken cancellationToken = default)
+        Task<int> RemoveItemAsync<TEntity, TId>(TId id, bool soft = false, CancellationToken cancellationToken = default)
             where TEntity : EntityBase, new()
             where TId : struct;
+
         /// <summary>
         /// Updates the item asynchronous.
         /// </summary>
         /// <typeparam name="TEntity">The type of the t entity.</typeparam>
         /// <typeparam name="TSource">The type of the t source.</typeparam>
         /// <param name="e">The e.</param>
-        /// <param name="user">The user.</param>
         /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <returns>Task&lt;System.Int32&gt;.</returns>
-        Task<int> UpdateItemAsync<TEntity, TSource>(TSource e, string user, CancellationToken cancellationToken = default)
+        Task<int> UpdateItemAsync<TEntity, TSource>(TSource e, CancellationToken cancellationToken = default)
             where TEntity : EntityBase, new()
             where TSource : ModelBase, new();
     }
