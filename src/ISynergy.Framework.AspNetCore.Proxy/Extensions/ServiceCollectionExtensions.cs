a05d5aec8464b73d67c7472cc4f4a6e1581a1643
Ismail Hassani <ismail.hassani@i-synergy.nl>
2022-10-17 01:43:14 +0200

    Fixed namespaces

--- src/ISynergy.Framework.AspNetCore.Proxy/Extensions/ServiceCollectionExtensions.cs
+++ src/ISynergy.Framework.AspNetCore.Proxy/Extensions/ServiceCollectionExtensions.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ISynergy.Framework.AspNetCore.Proxy.Extensions
+namespace ISynergy.Framework.AspNetCore.Extensions
 {
     /// <summary>
     /// Service collection extensions for proxy service
