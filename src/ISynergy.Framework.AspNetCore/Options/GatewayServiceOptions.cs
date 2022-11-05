db8b8c019c5c019aab4078abfcbfa0e5c738ae46
Ismail Hassani <ismail.hassani@i-synergy.nl>
2022-11-05 11:32:25 +0100

    Added Gateway service options

---
+++ src/ISynergy.Framework.AspNetCore/Options/GatewayServiceOptions.cs
@@ -0,0 +1,14 @@
+namespace ISynergy.Framework.AspNetCore.Options
+{
+    /// <summary>
+    /// Class GatewayServiceOption.
+    /// </summary>
+    public class GatewayServiceOptions
+    {
+        /// <summary>
+        /// Gets or sets the endpoint.
+        /// </summary>
+        /// <value>The home page.</value>
+        public string Endpoint { get; set; } = string.Empty;
+    }
+}
