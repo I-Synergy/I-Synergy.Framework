9363a14708408347168c083ff58917d64eae6fea
Ismail Hassani <ismail.hassani@i-synergy.nl>
2023-02-05 21:04:32 +0100

    Removed public properties in FileService to make it Singleton

--- src/ISynergy.Framework.Mvvm/Abstractions/Services/IFileService.cs
+++ src/ISynergy.Framework.Mvvm/Abstractions/Services/IFileService.cs
@@ -8,47 +8,6 @@ namespace ISynergy.Framework.Mvvm.Abstractions.Services
     public interface IFileService<T>
     {
         /// <summary>
-        /// Gets or sets the filter.
-        /// </summary>
-        /// <value>The filter.</value>
-        string Filter { get; set; }
-        /// <summary>
-        /// Gets or sets a value indicating whether [add extension].
-        /// </summary>
-        /// <value><c>true</c> if [add extension]; otherwise, <c>false</c>.</value>
-        bool AddExtension { get; set; }
-        /// <summary>
-        /// Gets or sets a value indicating whether [check file exists].
-        /// </summary>
-        /// <value><c>true</c> if [check file exists]; otherwise, <c>false</c>.</value>
-        bool CheckFileExists { get; set; }
-        /// <summary>
-        /// Gets or sets a value indicating whether [check path exists].
-        /// </summary>
-        /// <value><c>true</c> if [check path exists]; otherwise, <c>false</c>.</value>
-        bool CheckPathExists { get; set; }
-        /// <summary>
-        /// Gets or sets the index of the filter.
-        /// </summary>
-        /// <value>The index of the filter.</value>
-        int FilterIndex { get; set; }
-        /// <summary>
-        /// Gets or sets the initial directory.
-        /// </summary>
-        /// <value>The initial directory.</value>
-        string InitialDirectory { get; set; }
-        /// <summary>
-        /// Gets or sets the title.
-        /// </summary>
-        /// <value>The title.</value>
-        string Title { get; set; }
-        /// <summary>
-        /// Gets or sets a value indicating whether [validate names].
-        /// </summary>
-        /// <value><c>true</c> if [validate names]; otherwise, <c>false</c>.</value>
-        bool ValidateNames { get; set; }
-
-        /// <summary>
         /// Saves the file asynchronous.
         /// </summary>
         /// <param name="filename">The filename.</param>
