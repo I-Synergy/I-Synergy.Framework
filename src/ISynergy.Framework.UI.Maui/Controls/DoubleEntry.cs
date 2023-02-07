c7827e83583bf0283bbce005d5369e1bcbcd9258
Ismail Hassani <ismail.hassani@i-synergy.nl>
2023-02-08 00:49:31 +0100

    Fixed issue where numeric input caused a stack overflow.

---
+++ src/ISynergy.Framework.UI.Maui/Controls/DoubleEntry.cs
@@ -0,0 +1,39 @@
+﻿namespace ISynergy.Framework.UI.Controls
+{
+    public class DoubleEntry : NumericEntry<double>
+    {
+        public DoubleEntry() : base()
+        {
+        }
+
+        public override void NumericEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string separator = Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+
+            this.TextChanged -= NumericEntry_TextChanged;
+
+            if (e.NewTextValue.EndsWith(separator))
+                Text = $"{e.NewTextValue.Replace(separator, "")}{separator}";
+
+            else if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                Text = (0d).ToString();
+                Value = 0d;
+            }
+
+            else if (double.TryParse(e.NewTextValue.Trim(), out double newResult))
+            {
+                Text = newResult.ToString();
+                Value = newResult;
+            }
+
+            else if (double.TryParse(e.OldTextValue.Trim(), out double oldResult))
+            {
+                Text = oldResult.ToString();
+                Value = oldResult;
+            }
+
+            this.TextChanged += NumericEntry_TextChanged;
+        }
+    }
+}
