17765cc89bde6b1cad718052555224dd54248bf0
Ismail Hassani <ismail.hassani@i-synergy.nl>
2023-02-26 16:49:58 +0100

    Updated nuget packages

---
+++ src/ISynergy.Framework.UI.WPF/Converters/SelectionModeConverters.cs
@@ -0,0 +1,19 @@
+﻿using ISynergy.Framework.Mvvm.Enumerations;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ISynergy.Framework.UI.Converters
+{
+    public class SelectionModeConverters : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SelectionModes selectionMode && selectionMode == SelectionModes.Multiple)
+                return SelectionMode.Multiple;
+
+            return SelectionMode.Single;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
---
+++ src/ISynergy.Framework.UI.WPF/Windows/SelectionWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ISynergy.Framework.Mvvm.Abstractions.ViewModels;
+using ISynergy.Framework.Mvvm.Abstractions.Windows;
+using ISynergy.Framework.Mvvm.Enumerations;
+
+namespace ISynergy.Framework.UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for SelectionWindow.xaml
+    /// </summary>
+    public partial class SelectionWindow : ISelectionWindow
+    {
+        public SelectionWindow()
+        {
+            InitializeComponent();
+            DataContextChanged += this.SelectionWindow_DataContextChanged;
+        }
+
+        private void SelectionWindow_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            if (ViewModel is ISelectionViewModel viewModel)
+            {
+                if (viewModel.SelectionMode == SelectionModes.Single && viewModel.SelectedItems is not null && viewModel.SelectedItems.Count == 1)
+                {
+                    DataSummary.SelectedItem = viewModel.SelectedItems.Single();
+                }
+                else
+                {
+                    foreach (var item in viewModel.SelectedItems)
+                    {
+                        _ = DataSummary.Items.IndexOf(item);
+                    }
+                }
+            }
+        }
+
+        private void DataSummary_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ViewModel is ISelectionViewModel viewModel)
+            {
+                viewModel.SelectedItems = new List<object>();
+
+                if (viewModel.SelectionMode == SelectionModes.Single)
+                    viewModel.SelectedItems.Add(DataSummary.SelectedItem);
+                else
+                {
+                    foreach (var item in DataSummary.SelectedItems)
+                    {
+                        viewModel.SelectedItems.Add(item);
+                    }
+                }
+            }
+        }
+    }
+}
