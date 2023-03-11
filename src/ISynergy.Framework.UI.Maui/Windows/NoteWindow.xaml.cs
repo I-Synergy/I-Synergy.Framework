69527861e7525315f6f134668bf9cf08e8f27d98
Ismail Hassani <ismail.hassani@i-synergy.nl>
2023-03-11 20:04:09 +0100

    Added NoteWindow

---
+++ src/ISynergy.Framework.UI.Maui/Windows/NoteWindow.xaml.cs
@@ -0,0 +1,11 @@
+using ISynergy.Framework.Mvvm.Abstractions.Windows;
+
+namespace ISynergy.Framework.UI.Windows;
+
+public partial class NoteWindow : INoteWindow
+{
+	public NoteWindow()
+	{
+		InitializeComponent();
+	}
+}
