a62c852d0eb5bca8757ccde85d6c842443d14486
Ismail Hassani <ismail.hassani@i-synergy.nl>
2023-06-27 11:38:56 +0200

    Fixed issue where multiple windows in WPF where not opened

---
+++ samples/ISynergy.Framework.UI/Sample.WPF/Views/Test2Window.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ISynergy.Framework.Mvvm.Abstractions;
+
+namespace Sample.Views
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class Test2Window : IWindow
+    {
+        public Test2Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
