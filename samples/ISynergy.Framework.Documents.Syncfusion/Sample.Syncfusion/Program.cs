d3db207e3dc9b3ba73e93f9e511af519b7fe4242
Ismail Hassani <ismail.hassani@i-synergy.nl>
2023-07-27 15:46:03 -0700

    Updated nuget packages

---
+++ samples/ISynergy.Framework.Documents.Syncfusion/Sample.Syncfusion/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ISynergy.Framework.Documents.Extensions;
+using System;
+
+namespace Sample.Syncfusion
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            try
+            {
+                IConfigurationRoot config = new ConfigurationBuilder()
+                .Build();
+
+                ServiceProvider serviceProvider = new ServiceCollection()
+                    .AddLogging()
+                    .AddOptions()
+                    .AddDocumentsSyncfusionIntegration(config)
+                    .AddScoped<Startup>()
+                    .BuildServiceProvider();
+
+                Startup application = serviceProvider.GetRequiredService<Startup>();
+                await application.RunAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ samples/ISynergy.Framework.Documents.Syncfusion/Sample.Syncfusion/TestData.cs
@@ -0,0 +1,11 @@
+﻿namespace Sample.Syncfusion
+{
+    public class TestData
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public decimal Price { get; set; }  
+    }
+}
