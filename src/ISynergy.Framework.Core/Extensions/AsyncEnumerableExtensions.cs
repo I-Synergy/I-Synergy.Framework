d0137b13ed203b07ac548a2ca0f61ef889086495
Ismail Hassani <ismail.hassani@i-synergy.nl>
2023-09-15 02:32:34 +0200

    AsyncEnumerableExtensions

---
+++ src/ISynergy.Framework.Core/Extensions/AsyncEnumerableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using ISynergy.Framework.Core.Validation;
+
+namespace ISynergy.Framework.Core.Extensions
+{
+    /// <summary>
+    /// Class AsyncEnumerableExtensions.
+    /// </summary>
+    public static class AsyncEnumerableExtensions
+    {
+        /// <summary>
+        /// Converts to listasync.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns>Task&lt;List&lt;T&gt;&gt;.</returns>
+        public static Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> source)
+        {
+            Argument.IsNotNull(source);
+
+            return ExecuteAsync();
+
+            async Task<List<T>> ExecuteAsync()
+            {
+                var list = new List<T>();
+
+                await foreach (var element in source)
+                {
+                    list.Add(element);
+                }
+
+                return list;
+            }
+        }
+    }
+}
