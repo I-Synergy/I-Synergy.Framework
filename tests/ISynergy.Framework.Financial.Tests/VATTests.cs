20762c231af1611c17e6238967037bd6f52fb10c
Ismail Hassani <ismail.hassani@i-synergy.nl>
2023-12-02 12:09:08 +0100

    Fixed issue where VAT caculations where incorrect due to naming

--- tests/ISynergy.Framework.Financial.Tests/VATTests.cs
+++ tests/ISynergy.Framework.Financial.Tests/VATTests.cs
@@ -12,27 +12,27 @@ namespace ISynergy.Framework.Financial.Tests
         /// Defines the test method CalcPriceExclVATTest.
         /// </summary>
         [TestMethod]
-        public void CalcPriceExclVATTest()
+        public void CalculateAmountFromAmountExcludingVATTest()
         {
-            decimal result = VAT.CalculateAmountFromAmountExcludingVAT(21, 121);
-            Assert.AreEqual(100, result);
+            decimal result = VAT.CalculateAmountFromAmountExcludingVAT(21, 100);
+            Assert.AreEqual(121, result);
         }
 
         /// <summary>
         /// Defines the test method CalcPriceInclVATTest.
         /// </summary>
         [TestMethod]
-        public void CalcPriceInclVATTest()
+        public void CalculateAmountFromAmountIncludingVATTest()
         {
-            decimal result = VAT.CalculateAmountFromAmountIncludingVAT(21, 100);
-            Assert.AreEqual(121, result);
+            decimal result = VAT.CalculateAmountFromAmountIncludingVAT(21, 121);
+            Assert.AreEqual(100, result);
         }
 
         /// <summary>
         /// Defines the test method CalcVATExclVATTest.
         /// </summary>
         [TestMethod]
-        public void CalcVATExclVATTest()
+        public void CalculateVATFromAmountExcludingVATTest()
         {
             decimal result = VAT.CalculateVATFromAmountExcludingVAT(21, 100);
             Assert.AreEqual(21, result);
@@ -42,7 +42,7 @@ namespace ISynergy.Framework.Financial.Tests
         /// Defines the test method CalcVATInclVATTest.
         /// </summary>
         [TestMethod]
-        public void CalcVATInclVATTest()
+        public void CalculateVATFromAmountIncludingVATTest()
         {
             decimal result = VAT.CalculateVATFromAmountIncludingVAT(21, 121);
             Assert.AreEqual(21, result);
