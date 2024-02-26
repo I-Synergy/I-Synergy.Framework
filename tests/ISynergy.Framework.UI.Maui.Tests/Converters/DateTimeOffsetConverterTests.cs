23b2be06084b833c2560fab7d63c9365c4722248
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-02-26 15:18:27 +0100

    The `DateTimeOffsetConverterTests` class has been significantly updated. A new namespace, `System.Globalization`, has been added to the class. A new private readonly field, `_dateTimeOffsetToLocalDateTimeConverter`, of type `DateTimeOffsetToLocalDateTimeConverter` has been introduced. A constructor has been added to initialize this new field.
    Moreover, five new test methods have been added to the class. These methods test the `Convert` and `ConvertBack` methods of the `DateTimeOffsetToLocalDateTimeConverter` class under various conditions. All these test methods follow the Arrange-Act-Assert pattern for unit tests.
    
    Changes:
    
    1. Addition of `System.Globalization` namespace to `DateTimeOffsetConverterTests` class.
    2. Addition of a private readonly field `_dateTimeOffsetToLocalDateTimeConverter` of type `DateTimeOffsetToLocalDateTimeConverter` to `DateTimeOffsetConverterTests` class.
    3. Addition of a constructor to `DateTimeOffsetConverterTests` class to initialize `_dateTimeOffsetToLocalDateTimeConverter` field.
    4. Addition of `DateTimeOffsetToLocalDateTimeConverter_Convert_ShouldReturnLocalDateTime` test method to `DateTimeOffsetConverterTests` class.
    5. Addition of `DateTimeOffsetToLocalDateTimeConverter_Convert_ShouldReturnCurrentLocalDateTime_WhenValueIsNotDateTimeOffset` test method to `DateTimeOffsetConverterTests` class.
    6. Addition of `DateTimeOffsetToLocalDateTimeConverter_ConvertBack_ShouldReturnUniversalDateTimeOffset` test method to `DateTimeOffsetConverterTests` class.
    7. Addition of `DateTimeOffsetToLocalDateTimeConverter_ConvertBack_ShouldReturnCurrentUniversalDateTimeOffset_WhenValueIsNotDateTime` test method to `DateTimeOffsetConverterTests` class.

--- tests/ISynergy.Framework.UI.Maui.Tests/Converters/DateTimeOffsetConverterTests.cs
+++ tests/ISynergy.Framework.UI.Maui.Tests/Converters/DateTimeOffsetConverterTests.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
 
 namespace ISynergy.Framework.UI.Converters.Tests;
 
 [TestClass()]
 public class DateTimeOffsetConverterTests
 {
+    private readonly DateTimeOffsetToLocalDateTimeConverter _dateTimeOffsetToLocalDateTimeConverter;
+
+    public DateTimeOffsetConverterTests()
+    {
+        _dateTimeOffsetToLocalDateTimeConverter = new DateTimeOffsetToLocalDateTimeConverter();
+    }
+
     /// Create a unit test for <see cref="DateTimeOffsetConverter.DateTimeOffsetToTimeSpan(DateTimeOffset)"/> method.
     [TestMethod]
     public void DateTimeOffsetToTimeSpanTest()
@@ -28,5 +36,73 @@ public class DateTimeOffsetConverterTests
         DateTimeOffset? result = DateTimeOffsetConverter.TimeSpanToDateTimeOffset(dateTimeOffset, timeSpan);
         // Assert
         Assert.AreEqual(new DateTimeOffset(1975, 10, 29, 1, 2, 3, TimeSpan.Zero), result);
+    }
+
+    [TestMethod]
+    public void DateTimeOffsetToLocalDateTimeConverter_Convert_ShouldReturnLocalDateTime()
+    {
+        // Arrange
+        var dateTimeOffset = new DateTimeOffset(2022, 1, 1, 12, 0, 0, TimeSpan.Zero);
+
+        // Act
+        var result = _dateTimeOffsetToLocalDateTimeConverter.Convert(dateTimeOffset, typeof(DateTime), null, CultureInfo.InvariantCulture);
+
+        // Assert
+        Assert.IsInstanceOfType<DateTime>(result);
+        Assert.AreEqual(dateTimeOffset.ToLocalTime().DateTime, result);
+    }
+
+    [TestMethod]
+    public void DateTimeOffsetToLocalDateTimeConverter_Convert_ShouldReturnCurrentLocalDateTime_WhenValueIsNotDateTimeOffset()
+    {
+        // Arrange
+        var expectedDateTime = DateTime.Now.ToLocalTime();
+
+        // Act
+        var result = _dateTimeOffsetToLocalDateTimeConverter.Convert("invalid value", typeof(DateTime), null, CultureInfo.InvariantCulture);
+
+        // Assert
+        Assert.IsInstanceOfType<DateTime>(result);
+        Assert.AreEqual(expectedDateTime.Year, ((DateTime)result).Year);
+        Assert.AreEqual(expectedDateTime.Month, ((DateTime)result).Month);
+        Assert.AreEqual(expectedDateTime.Day, ((DateTime)result).Day);
+        Assert.AreEqual(expectedDateTime.Hour, ((DateTime)result).Hour);
+        Assert.AreEqual(expectedDateTime.Minute, ((DateTime)result).Minute);
+    }
+
+    [TestMethod]
+    public void DateTimeOffsetToLocalDateTimeConverter_ConvertBack_ShouldReturnUniversalDateTimeOffset()
+    {
+        // Arrange
+        var dateTime = new DateTime(2022, 1, 1, 12, 0, 0);
+
+        // Act
+        var result = _dateTimeOffsetToLocalDateTimeConverter.ConvertBack(dateTime, typeof(DateTimeOffset), null, CultureInfo.InvariantCulture);
+
+        // Assert
+        Assert.IsInstanceOfType<DateTimeOffset>(result);
+        Assert.AreEqual(dateTime.ToUniversalTime().Year, ((DateTimeOffset)result).Year);
+        Assert.AreEqual(dateTime.ToUniversalTime().Month, ((DateTimeOffset)result).Month);
+        Assert.AreEqual(dateTime.ToUniversalTime().Day, ((DateTimeOffset)result).Day);
+        Assert.AreEqual(dateTime.ToUniversalTime().Hour, ((DateTimeOffset)result).Hour);
+        Assert.AreEqual(dateTime.ToUniversalTime().Minute, ((DateTimeOffset)result).Minute);
+    }
+
+    [TestMethod]
+    public void DateTimeOffsetToLocalDateTimeConverter_ConvertBack_ShouldReturnCurrentUniversalDateTimeOffset_WhenValueIsNotDateTime()
+    {
+        // Arrange
+        var expectedDateTimeOffset = DateTimeOffset.Now.ToUniversalTime();
+
+        // Act
+        var result = _dateTimeOffsetToLocalDateTimeConverter.ConvertBack("invalid value", typeof(DateTimeOffset), null, CultureInfo.InvariantCulture);
+
+        // Assert
+        Assert.IsInstanceOfType<DateTimeOffset>(result);
+        Assert.AreEqual(expectedDateTimeOffset.Year, ((DateTimeOffset)result).Year);
+        Assert.AreEqual(expectedDateTimeOffset.Month, ((DateTimeOffset)result).Month);
+        Assert.AreEqual(expectedDateTimeOffset.Day, ((DateTimeOffset)result).Day);
+        Assert.AreEqual(expectedDateTimeOffset.Hour, ((DateTimeOffset)result).Hour);
+        Assert.AreEqual(expectedDateTimeOffset.Minute, ((DateTimeOffset)result).Minute);
     }
 }
