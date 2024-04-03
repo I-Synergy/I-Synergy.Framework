0026fb794f393e264c9687773eacab09ed3908d8
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-04-03 03:02:33 +0200

    The most significant changes involve the refactoring of methods in the `ExceptionExtensions.cs` file and the addition of a new test class in the `ExceptionExtensionsTests.cs` file.
    1. The `ExceptionExtensions.cs` file has been updated to include additional `using` statements for `System.Diagnostics`, `System.Linq.Expressions`, and `System.Reflection`. This change allows the file to access classes and methods from these namespaces.
    
    2. The `ToMessage` method in the `ExceptionExtensions.cs` file has been refactored to use the null-coalescing operator (`??=`) to initialize `stackTraceLines` if it is null. This change simplifies the code by replacing the previous `if` statement that checked for null.
    
    3. The `GetStackTraceLines` method in the `ExceptionExtensions.cs` file has been updated to handle a null `stackTrace` string. If `stackTrace` is null, a new `List<string>` is returned. This change ensures that the method can handle null inputs without throwing an exception.
    
    4. The `GetUserStackTraceLines` method in the `ExceptionExtensions.cs` file has been refactored to remove the use of a regular expression for filtering stack trace lines. Instead, the `EnsureNotNull` extension method is used to iterate over the stack trace lines. This change improves the performance of the method by avoiding the use of regular expressions.
    
    5. A new test class `ExceptionExtensionsTests` has been added in the `ExceptionExtensionsTests.cs` file. This class contains three test methods that test the `ToMessage` method under different conditions. The addition of this test class improves the test coverage of the `ExceptionExtensions.cs` file.

---
+++ tests/ISynergy.Framework.Core.Tests/Extensions/ExceptionExtensionsTests.cs
@@ -0,0 +1,60 @@
+using ISynergy.Framework.Core.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ISynergy.Framework.Core.Tests.Extensions
+{
+    [TestClass]
+    public class ExceptionExtensionsTests
+    {
+        [TestMethod]
+        public void ToMessage_ReturnsCorrectLogMessage()
+        {
+            string logMessage = null;
+
+            // Arrange
+            try
+            {
+                throw new Exception("Test Exception");
+            }
+            catch (Exception ex)
+            {
+                logMessage = ex.ToMessage(Environment.StackTrace);
+
+                Assert.IsTrue(logMessage.Contains(ex.Message));
+                Assert.IsNotNull(Environment.StackTrace);
+                Assert.IsTrue(logMessage.Contains(Environment.StackTrace));
+            }
+        }
+
+        [TestMethod]
+        public void ToMessage_ReturnsLogMessageWithoutEnvironmentStackTrace()
+        {
+            // Arrange
+            var exception = new Exception("Test Exception");
+            var environmentStackTrace = "";
+
+            // Act
+            var logMessage = exception.ToMessage(environmentStackTrace);
+
+            // Assert
+            Assert.IsTrue(logMessage.Contains(exception.Message));
+            Assert.IsTrue(string.IsNullOrEmpty(environmentStackTrace));
+            Assert.IsTrue(logMessage.Contains(environmentStackTrace));
+        }
+
+        [TestMethod]
+        public void ToMessage_ReturnsLogMessageWithNullEnvironmentStackTrace()
+        {
+            // Arrange
+            var exception = new Exception("Test Exception");
+            string environmentStackTrace = null;
+
+            // Act
+            var logMessage = exception.ToMessage(environmentStackTrace);
+
+            // Assert
+            Assert.IsTrue(logMessage.Contains(exception.Message));
+            Assert.IsTrue(string.IsNullOrEmpty(environmentStackTrace));
+        }
+    }
+}
