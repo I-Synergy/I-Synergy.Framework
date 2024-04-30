4248936a5540fa607fb4b4be0e916a5e7a05d1af
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-04-30 11:53:20 +0200

    The most significant changes include the update of several package versions in `Directory.Packages.props`, the addition of a new extension method to the `ConfigurationManager` class, and the introduction of two new classes, `IntrospectionOptions` and `AzureStorageOptions`.
    1. The versions of several packages in `Directory.Packages.props` have been updated. The packages include `Microsoft.Graph`, `Microsoft.Maui.Controls`, `Microsoft.Maui.Controls.Compatibility`, and all `Syncfusion` packages. This change ensures that the project is using the latest versions of these packages, which may include important bug fixes, security updates, and new features.
    
    2. A new extension method `GetSetting` has been added to the `ConfigurationManager` class in `ConfigurationManagerExtensions.cs`. This method allows for the retrieval of a configuration section based on a provided key, which can simplify the process of accessing configuration data.
    
    3. A new class `IntrospectionOptions` has been added in `IntrospectionOptions.cs`. This class contains properties for `Issuer`, `ClientId`, and `ClientSecret`. These properties are typically used in the context of OAuth 2.0 and OpenID Connect for authenticating and authorizing clients.
    
    4. A new class `AzureStorageOptions` has been added in `AzureStorageOptions.cs`. This class implements the `IStorageOptions` interface and contains a property for `ConnectionString`. This class can be used to configure and manage connections to Azure Storage services.

---
+++ src/ISynergy.Framework.AspNetCore/Extensions/ConfigurationManagerExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ISynergy.Framework.AspNetCore.Extensions;
+public static class ConfigurationManagerExtensions
+{
+    public static IConfigurationSection GetSetting(this ConfigurationManager configuration, string key) =>
+        configuration.GetSection($"Settings:{key}");
+}
---
+++ src/ISynergy.Framework.Storage/Options/AzureStorageOptions.cs
@@ -0,0 +1,13 @@
+﻿using ISynergy.Framework.Storage.Abstractions.Options;
+
+namespace ISynergy.Framework.Storage.Options;
+/// <summary>
+/// Azure Storage options.
+/// </summary>
+public class AzureStorageOptions : IStorageOptions
+{
+    /// <summary>
+    /// Connectionstring
+    /// </summary>
+    public string ConnectionString { get; set; }
+}
