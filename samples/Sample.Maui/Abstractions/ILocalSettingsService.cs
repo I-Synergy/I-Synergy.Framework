5c572b6015ef02ef6af6f5b41c78e0e14f60fa96
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-05-23 19:53:41 +0200

    Refactor LocalSettings and related services
    This commit includes a major refactoring of the `LocalSettingsService` and related classes. The `ILocalSettingsService` interface now inherits from `IApplicationSettingsService` and a new `SynchronizationSettings` property has been added. The `LocalSettings` class now implements `IApplicationSettings` with several synchronization-related properties removed.
    
    Significant changes have been made to `LocalSettingsService`, including a new constructor parameter (`IPreferences`), updated `LoadSettings` and `SaveSettings` methods, and a split of the `_settings` field into `_localSettings` and `_synchronizationSettings`. The service now implements `IApplicationSettingsService` and exposes `LocalSettings` and `SynchronizationSettings` properties.
    
    File handling code has been removed, indicating a shift from file-based settings storage to a different mechanism, possibly provided by the `IPreferences` object. The `LocalSettingsService` no longer implements `ISynchronizationSettingsService` and `IBaseApplicationSettingsService`, but instead implements `IApplicationSettingsService`.

--- samples/Sample.Maui/Abstractions/ILocalSettingsService.cs
+++ samples/Sample.Maui/Abstractions/ILocalSettingsService.cs
@@ -1,8 +1,11 @@
-﻿using ISynergy.Framework.Synchronization.Abstractions;
+﻿using ISynergy.Framework.Core.Abstractions.Services.Base;
+using ISynergy.Framework.Synchronization.Options;
 using Sample.Models;
 
 namespace Sample.Abstractions;
-public interface ILocalSettingsService : ISynchronizationSettingsService
+
+public interface ILocalSettingsService : IApplicationSettingsService
 {
     new LocalSettings Settings { get; }
+    SynchronizationSettings SynchronizationSettings { get; }
 }
