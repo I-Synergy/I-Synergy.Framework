befd6e78c3cfceb69fdf0d32bd6b059f169a6380
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-05-24 12:08:41 +0200

    Removed ISynergy.Framework.Mvvm reference from ISynergy.Framework.Synchronization
    Removed the project reference to `ISynergy.Framework.Mvvm.csproj` from the `ISynergy.Framework.Synchronization.csproj` file. This change eliminates the dependency of the `ISynergy.Framework.Synchronization` project on the `ISynergy.Framework.Mvvm` project.

--- samples/Sample.Synchronization/Controllers/SyncController.cs
+++ samples/Sample.Synchronization/Controllers/SyncController.cs
@@ -4,7 +4,6 @@ using Dotmim.Sync.SqlServer;
 using Dotmim.Sync.Web.Server;
 using ISynergy.Framework.Core.Serializers;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Serialization;
 
 namespace Sample.Synchronization.Controllers
 {
