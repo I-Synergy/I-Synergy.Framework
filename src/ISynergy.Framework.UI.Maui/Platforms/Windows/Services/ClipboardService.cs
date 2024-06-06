96fa9892a258940c177f51c453aaf5b741d2342a
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-06-06 13:56:51 +0200

    Added clipboard service for Maui

---
+++ src/ISynergy.Framework.UI.Maui/Platforms/Windows/Services/ClipboardService.cs
@@ -0,0 +1,27 @@
+﻿using ISynergy.Framework.Core.Extensions;
+using ISynergy.Framework.Core.Models.Results;
+using ISynergy.Framework.Mvvm.Abstractions.Services;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace ISynergy.Framework.UI.Services;
+
+public class ClipboardService : IClipboardService
+{
+    public async Task<ImageResult> GetImageFromClipboardAsync()
+    {
+        var dataPackageView = global::Windows.ApplicationModel.DataTransfer.Clipboard.GetContent();
+
+        if (dataPackageView.Contains(StandardDataFormats.Bitmap) &&
+            await dataPackageView.GetBitmapAsync() is { } imageReceived)
+        {
+            using var imageStream = await imageReceived.OpenReadAsync();
+            var image = await imageStream.AsStreamForRead().ToByteArrayAsync();
+
+            if (imageStream.ContentType == "image/bmp")
+                return new ImageResult(image.ToImageBytes(100, System.Drawing.Imaging.ImageFormat.Png), "image/png");
+            return new ImageResult(image, imageStream.ContentType);
+        }
+
+        return null;
+    }
+}
