3568e646def126402bea9b01ac7d5a87c375f512
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-09-05 11:13:37 +0200

    Refactor AzureOptions to use KeyVaultOptions class
    Refactored the `AzureOptions` class to replace the nested `KeyVault` class with a property of type `KeyVaultOptions`. The nested `KeyVault` class, which contained properties for `KeyVaultUri`, `ClientId`, and `ClientSecret`, has been removed. Added a new file `KeyVaultOptions.cs` defining the `KeyVaultOptions` class with properties for `KeyVaultUri`, `ClientId`, and `ClientSecret`.

--- src/ISynergy.Framework.AspNetCore/Options/AzureOptions.cs
+++ src/ISynergy.Framework.AspNetCore/Options/AzureOptions.cs
@@ -8,24 +8,7 @@ public class AzureOptions
     public string TenantId { get; set; }
 
     /// <summary>
-    /// Class KeyVault.
+    /// Gets or sets the keyvault.
     /// </summary>
-    public class KeyVault
-    {
-        /// <summary>
-        /// Gets or sets the key vault URI.
-        /// </summary>
-        /// <value>The key vault URI.</value>
-        public Uri KeyVaultUri { get; set; }
-        /// <summary>
-        /// Gets or sets the client identifier.
-        /// </summary>
-        /// <value>The client identifier.</value>
-        public string ClientId { get; set; }
-        /// <summary>
-        /// Gets or sets the client secret.
-        /// </summary>
-        /// <value>The client secret.</value>
-        public string ClientSecret { get; set; }
-    }
+    public KeyVaultOptions KeyVault { get; set; }
 }
