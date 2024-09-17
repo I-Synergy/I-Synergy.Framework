ca66ed52784494c82f078a66cc6e7da9683e95b3
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-09-17 12:52:54 +0200

    Upgrade packages and refactor method parameters
    Upgraded `VideoLAN.LibVLC.Windows` to `3.0.21` in `Sample.Uno.csproj`.
    Renamed `page` to `pageIndex` in `EnumerableExtensions.ToPage`.
    Corrected `pageSize` casing in `QueryableExtensions` methods.
    Upgraded `CommunityToolkit.WinUI.Behaviors` to `8.1.240916`.
    Refactored `VATTests` to use `DataTestMethod` and `DataRow`.

--- src/ISynergy.Framework.EntityFramework/Extensions/QueryableExtensions.cs
+++ src/ISynergy.Framework.EntityFramework/Extensions/QueryableExtensions.cs
@@ -6,20 +6,20 @@
 public static class QueryableExtensions
 {
     /// <summary>
-    /// Counts to pages of entities according to a certain pagesize.
+    /// Counts to pages of entities according to a certain pageSize.
     /// </summary>
     /// <typeparam name="TEntity">The type of the t entity.</typeparam>
     /// <param name="query">The query.</param>
-    /// <param name="pagesize">Has to be greater than 0.</param>
+    /// <param name="pageSize">Has to be greater than 0.</param>
     /// <returns>System.Int32.</returns>
-    /// <exception cref="ArgumentOutOfRangeException">Value must be greater than 0. - pagesize</exception>
-    public static int CountPages<TEntity>(this IQueryable<TEntity> query, int pagesize)
+    /// <exception cref="ArgumentOutOfRangeException">Value must be greater than 0. - pageSize</exception>
+    public static int CountPages<TEntity>(this IQueryable<TEntity> query, int pageSize)
     {
-        if (pagesize < 1)
-            throw new ArgumentOutOfRangeException(nameof(pagesize), "Value must be greater than 0.");
+        if (pageSize < 1)
+            throw new ArgumentOutOfRangeException(nameof(pageSize), "Value must be greater than 0.");
 
         var countPages = Convert.ToDecimal(query.Count());
-        return Convert.ToInt32(Math.Ceiling(countPages / pagesize));
+        return Convert.ToInt32(Math.Ceiling(countPages / pageSize));
     }
 
     /// <summary>
@@ -28,20 +28,20 @@ public static class QueryableExtensions
     /// </summary>
     /// <typeparam name="TEntity">The type of the t entity.</typeparam>
     /// <param name="query">The query.</param>
-    /// <param name="page">Has to be non-negative.</param>
-    /// <param name="pagesize">Has to be greater than 0.</param>
+    /// <param name="pageIndex">Has to be non-negative.</param>
+    /// <param name="pageSize">Has to be greater than 0.</param>
     /// <returns>IQueryable&lt;TEntity&gt;.</returns>
-    /// <exception cref="ArgumentOutOfRangeException">Value must be non-negative. - page</exception>
-    /// <exception cref="ArgumentOutOfRangeException">Value must be greater than 0. - pagesize</exception>
-    public static IQueryable<TEntity> ToPage<TEntity>(this IQueryable<TEntity> query, int page, int pagesize)
+    /// <exception cref="ArgumentOutOfRangeException">Value must be non-negative. - pageIndex</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Value must be greater than 0. - pageSize</exception>
+    public static IQueryable<TEntity> ToPage<TEntity>(this IQueryable<TEntity> query, int pageIndex, int pageSize)
     {
-        if (page < 0)
-            throw new ArgumentOutOfRangeException(nameof(page), "Value must be non-negative.");
-        if (pagesize < 1)
-            throw new ArgumentOutOfRangeException(nameof(pagesize), "Value must be greater than 0.");
+        if (pageIndex < 0)
+            throw new ArgumentOutOfRangeException(nameof(pageIndex), "Value must be non-negative.");
+        if (pageSize < 1)
+            throw new ArgumentOutOfRangeException(nameof(pageSize), "Value must be greater than 0.");
 
         return query
-            .Skip((page - 1) * pagesize)
-            .Take(pagesize);
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize);
     }
 }
