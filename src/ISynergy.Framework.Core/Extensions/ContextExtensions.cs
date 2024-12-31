90cd97f12435e1fba3079c91770592cd826d904c
Ismail Hassani <ismail.hassani@i-synergy.nl>
2024-12-31 13:52:14 +0000

    Add ContextExtensions with ToEnvironmentalRefreshToken method
    Introduced new using directives and namespace `ISynergy.Framework.Core.Extensions`. Created a static class `ContextExtensions` with an extension method `ToEnvironmentalRefreshToken` for the `IContext` interface. This method retrieves and modifies the `RefreshToken` based on the `Environment` property of the `IContext` instance, prefixing it with `GenericConstants.UsernamePrefixTest` for `Test` environment and `GenericConstants.UsernamePrefixLocal` for `Local` environment.

---
+++ src/ISynergy.Framework.Core/Extensions/ContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ISynergy.Framework.Core.Abstractions;
+using ISynergy.Framework.Core.Constants;
+using ISynergy.Framework.Core.Enumerations;
+
+namespace ISynergy.Framework.Core.Extensions;
+
+public static class ContextExtensions
+{
+    public static string ToEnvironmentalRefreshToken(this IContext context)
+    {
+        var token = context.Profile.Token.RefreshToken;
+        return context.Environment switch
+        {
+            SoftwareEnvironments.Test => $"{GenericConstants.UsernamePrefixTest}{token}",
+            SoftwareEnvironments.Local => $"{GenericConstants.UsernamePrefixLocal}{token}",
+            _ => token,
+        };
+    }
+}
