fc195d112f4616dae1b7da386053e54bb5df025f
Ismail Hassani <ismail.hassani@i-synergy.nl>
2025-01-13 18:28:00 +0100

    Implement IDisposable in BladeView for resource management
    Introduce IDisposable interface to BladeView class to manage and release resources. Implement Dispose method to clean up managed resources, including detaching event handlers. Use conditional compilation directives for platform-specific Dispose method implementation. Ensure class structure and properties remain unchanged for continued functionality.

--- src/ISynergy.Framework.UI.WinUI/Controls/BladeView/BladeView.xaml.cs
+++ src/ISynergy.Framework.UI.WinUI/Controls/BladeView/BladeView.xaml.cs
@@ -7,7 +7,7 @@ using System.Collections.ObjectModel;
 
 namespace ISynergy.Framework.UI.Controls;
 
-public partial class BladeView : UserControl
+public partial class BladeView : UserControl, IDisposable
 {
     public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(nameof(ItemsSource), typeof(ObservableCollection<UIElement>), typeof(BladeView), new PropertyMetadata(new ObservableCollection<UIElement>()));
 
@@ -143,4 +143,72 @@ public partial class BladeView : UserControl
             }
         }
     }
+
+    #region IDisposable
+    // Dispose() calls Dispose(true)
+#if WINDOWS
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+#else
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+    /// </summary>
+    public new void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+        base.Dispose();
+    }
+#endif
+
+    // The bulk of the clean-up code is implemented in Dispose(bool)
+
+#if IOS || MACCATALYST || ANDROID
+    /// <summary>
+    /// Releases unmanaged and - optionally - managed resources.
+    /// </summary>
+    /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+    protected virtual new void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            // free managed resources
+            if (this.DataContext is IViewModelBladeView bladeView && bladeView.Blades is not null)
+                bladeView.Blades.CollectionChanged -= Blades_CollectionChanged;
+
+            this.DataContextChanged -= BladeView_DataContextChanged;
+            this.SizeChanged -= BladeViewEx_SizeChanged;
+        }
+
+        // free native resources if there are any.
+
+        base.Dispose(disposing);
+    }
+#else
+    /// <summary>
+    /// Releases unmanaged and - optionally - managed resources.
+    /// </summary>
+    /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            // free managed resources
+            if (this.DataContext is IViewModelBladeView bladeView && bladeView.Blades is not null)
+                bladeView.Blades.CollectionChanged -= Blades_CollectionChanged;
+
+            this.DataContextChanged -= BladeView_DataContextChanged;
+            this.SizeChanged -= BladeViewEx_SizeChanged;
+        }
+
+        // free native resources if there are any.
+    }
+#endif
+    #endregion
 }
