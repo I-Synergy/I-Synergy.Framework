1d0f11375ad6b00d0cd777caffa166698af867c3
Ismail Hassani <ismail.hassani@i-synergy.nl>
2025-09-10 02:03:59 +0200

    Update .net 10 and c# 14

--- src/ISynergy.Framework.AspNetCore/Extensions/ResultExtensions.cs
+++ src/ISynergy.Framework.AspNetCore/Extensions/ResultExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using ISynergy.Framework.Core.Abstractions;
 using ISynergy.Framework.Core.Models.Results;
+using ISynergy.Framework.Core.Serializers;
 using System.Text.Json;
 
 namespace ISynergy.Framework.AspNetCore.Extensions;
@@ -9,21 +10,21 @@ public static class ResultExtensions
     public static async Task<IResult<T>?> ToResult<T>(this HttpResponseMessage response)
     {
         var responseAsString = await response.Content.ReadAsStringAsync();
-        var responseObject = JsonSerializer.Deserialize<Result<T>>(responseAsString, JsonSerializerOptions.Web);
+        var responseObject = JsonSerializer.Deserialize<Result<T>>(responseAsString, DefaultJsonSerializers.Web);
         return responseObject;
     }
 
     public static async Task<IResult?> ToResult(this HttpResponseMessage response)
     {
         var responseAsString = await response.Content.ReadAsStringAsync();
-        var responseObject = JsonSerializer.Deserialize<Result>(responseAsString, JsonSerializerOptions.Web);
+        var responseObject = JsonSerializer.Deserialize<Result>(responseAsString, DefaultJsonSerializers.Web);
         return responseObject;
     }
 
     public static async Task<PaginatedResult<T>?> ToPaginatedResult<T>(this HttpResponseMessage response)
     {
         var responseAsString = await response.Content.ReadAsStringAsync();
-        var responseObject = JsonSerializer.Deserialize<PaginatedResult<T>>(responseAsString, JsonSerializerOptions.Web);
+        var responseObject = JsonSerializer.Deserialize<PaginatedResult<T>>(responseAsString, DefaultJsonSerializers.Web);
         return responseObject;
     }
 }
